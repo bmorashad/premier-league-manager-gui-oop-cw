d84f894474d2603f22fd91d9de7915f6c0d7796f
bsucker98 <bmorashad@gmail.com>
2020-11-29 11:44:31 +0530

    v22: working version

--- src/app/views/professional-league/professional-league.component.ts
+++ src/app/views/professional-league/professional-league.component.ts
@@ -10,10 +10,73 @@ import { IFootballClub } from '../../dto/FootballClub';
   styleUrls: ['./professional-league.component.css']
 })
 export class ProfessionalLeagueComponent implements OnInit {
+	errorNotify: boolean = false;
+	errorNotifyMessage: string = "";
+	successNotify: boolean = false;
+	successMessage: string = "match created successfully";
+	matchGeneratedTitle: string = "Match generated successfully"
+	matchGenerateErrorTitle: string = "ERROR: Generated Match Failed :/"
+	errorMessage: string = ""
+	generatedMatch: IMatch = null;
+	showGeneratedMatch: boolean = false;
+	showGeneratedMatchError: boolean = false;
+
 	footballClubs: IFootballClub[] = [];
 	matches: IMatch[] = [];
 
-  constructor(private footballClubService: FootballClubService, private matchService: MatchService) { }
+	onCloseError() : void {
+		this.toggleGeneratedMatchError();
+	}
+	onMatchGenerate(match: IMatch) : void {
+		this.generatedMatch = match;
+		if(match == null) {
+			this.setErrorMessage("not enough clubs to generate match");
+			return this.toggleGeneratedMatchError();
+		}
+		return this.toggleGeneratedMatchModal();
+	}
+	confirmGeneratedMatch(response: boolean) : void {
+		if(response) {
+			this.matchService.create(this.generatedMatch).
+				subscribe(res => {
+				if (res.status == 1) {
+					this.matches = this.addMatch(this.generatedMatch);
+					this.showSuccessNotify();
+				} else  {
+					if(res.status == 0) {
+						this.setErrorNotifyMessage(res.errorMessage);
+					}
+					this.showErrorNotify();
+				} }, () => {
+					this.setErrorNotifyMessage("something went wrong on server!")
+					this.showErrorNotify();
+				});
+		}
+		this.toggleGeneratedMatchModal()
+	}
+	addMatch(match: IMatch): IMatch[] {
+		return [ ...this.matches, match];
+	}
+	showErrorNotify() {
+		this.errorNotify = true;
+	}
+	showSuccessNotify() {
+		this.successNotify = true;
+	}
+	setErrorMessage(message: string) : void{
+		this.errorMessage = message;
+	}
+	setErrorNotifyMessage(message: string) : void{
+		this.errorNotifyMessage = message;
+	}
+	// close or open
+	toggleGeneratedMatchModal() {
+		this.showGeneratedMatch = !this.showGeneratedMatch;
+	}
+	toggleGeneratedMatchError() { 
+		this.showGeneratedMatchError = !this.showGeneratedMatchError;
+	}
+	constructor(private footballClubService: FootballClubService, private matchService: MatchService) { }
 
   loadMatches() {
 	  this.matchService.all()
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommonUiModule } from '../common-ui/common-ui.module'
+import { GeneratedMatchComponent } from './generated-match/generated-match.component';
 import { ProfessionalLeagueComponent } from './professional-league/professional-league.component';
 import { SchoolDivisionLeagueComponent } from './school-division-league/school-division-league.component';
 import { UniDivisionLeagueComponent } from './uni-division-league/uni-division-league.component';
@@ -8,10 +10,11 @@ import { PremierLeagueModule } from '../premier-league/premier-league.module';
 
 
 @NgModule({
-  declarations: [ProfessionalLeagueComponent, SchoolDivisionLeagueComponent, UniDivisionLeagueComponent],
+  declarations: [ProfessionalLeagueComponent, SchoolDivisionLeagueComponent, UniDivisionLeagueComponent, GeneratedMatchComponent],
   imports: [
     CommonModule,
-	PremierLeagueModule
+	PremierLeagueModule,
+	CommonUiModule
   ],
   exports: [
 	  ProfessionalLeagueComponent,
