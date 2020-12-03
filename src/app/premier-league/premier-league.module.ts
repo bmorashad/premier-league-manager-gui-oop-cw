6b1c9a276da4d82c45076d19fa0a90554f6a97c0
bsucker98 <bmorashad@gmail.com>
2020-12-03 21:58:29 +0530

    v3: major bug fix

--- src/app/premier-league/premier-league.module.ts
+++ src/app/premier-league/premier-league.module.ts
@@ -5,11 +5,12 @@ import { PremierLeagueComponent } from './premier-league.component';
 import { CommonUiModule } from '../common-ui/common-ui.module';
 import { MatchDetailCardComponent } from './match-detail-card/match-detail-card.component';
 import { MatchListComponent } from './match-list/match-list.component';
+import { GeneratedMatchComponent } from './generated-match/generated-match.component'
 
 
 
 @NgModule({
-  declarations: [StandingsTableComponent, PremierLeagueComponent, MatchDetailCardComponent, MatchListComponent],
+  declarations: [StandingsTableComponent, PremierLeagueComponent, MatchDetailCardComponent, MatchListComponent, GeneratedMatchComponent],
   imports: [
     CommonModule,
 	CommonUiModule
