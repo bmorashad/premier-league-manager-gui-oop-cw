80272a1086aebe2d7118922813d0d236c91c18ff
bsucker98 <bmorashad@gmail.com>
2020-12-04 15:25:35 +0530

    animation successfully added snackbar

--- src/app/common-ui/snackbar/snackbar.component.ts
+++ src/app/common-ui/snackbar/snackbar.component.ts
@@ -1,9 +1,31 @@
 import { Component, Input, Output, OnChanges, OnDestroy, EventEmitter } from '@angular/core';
-
+import {
+	trigger,
+	state,
+	style,
+	animate,
+	transition
+} from '@angular/animations'
 @Component({
 	selector: 'snackbar',
 	templateUrl: './snackbar.component.html',
-	styleUrls: ['./snackbar.component.css']
+	styleUrls: ['./snackbar.component.css'],
+	animations: [
+		trigger('popOverState', [
+			state('show', style({
+				transform: 'translateY(0)',
+				opacity: '1',
+				visibility: 'visible' 
+			})),
+			state('hide', style({
+				transform: 'translateY(-100%)',
+				opacity: '0',
+				visibility: 'hidden' 
+			})),
+			transition('hide => show', animate('100ms ease-in') ),
+			transition('show => hide', animate('100ms ease-out'))
+		])
+	]
 })
 export class SnackbarComponent implements OnChanges, OnDestroy{
 	defaultType: string = "info";
@@ -13,6 +35,9 @@ export class SnackbarComponent implements OnChanges, OnDestroy{
 	availableTypes: string[] = ["info", "success", "error"];
 	timer: ReturnType<typeof setTimeout>;
 
+	get showState() {
+		return this.show ? 'show' : 'hide'
+	}
 	onClick(): void {
 		this.clearTimer();
 		this.hideBar();
