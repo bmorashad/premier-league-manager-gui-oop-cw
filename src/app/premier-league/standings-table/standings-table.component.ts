d159a18ca1310a668cf9bb0ef87526fb71cc5c41
bsucker98 <bmorashad@gmail.com>
2020-12-06 00:03:23 +0530

    added form column to standings table

--- src/app/premier-league/standings-table/standings-table.component.ts
+++ src/app/premier-league/standings-table/standings-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { IFootballClub } from '../../dto/FootballClub';
 import { trigger, style, animate, transition, query, stagger } from '@angular/animations';
+import {IMatch} from 'src/app/dto/Match';
 
 @Component({
 	selector: 'standings-table',
@@ -19,10 +20,11 @@ import { trigger, style, animate, transition, query, stagger } from '@angular/an
 })
 export class StandingsTableComponent implements OnInit, OnChanges{
 	@Input() footballClubs: IFootballClub[] = [];
+	@Input() matches: IMatch[] = [];
 	@Input() _id: string;
 	@Input() sortBy: string = "POINTS";
 
-	fields: string[] = ["Club", "MP", "W", "D", "L", "GS", "GA", "GF", "Pts"]
+	fields: string[] = ["Club", "MP", "W", "D", "L", "GS", "GA", "GF", "Pts", "Form"]
 
 	ngOnInit() : void {
 		this.sortClubs(this.footballClubs, this.sortBy);
@@ -49,6 +51,45 @@ export class StandingsTableComponent implements OnInit, OnChanges{
 	}
 	sortClubsByMatches(footballClubs: IFootballClub[]) : IFootballClub[] {
 		return footballClubs.sort((clubA, clubB) => clubB.matchCount - clubA.matchCount)
+	}
+
+	isClubInMatch(match: IMatch, footballClub: IFootballClub) {
+		return match.teamA == footballClub.clubName || 
+			match.teamB == footballClub.clubName
+	}
+	getMatchesByClub(footballClub: IFootballClub, limit ?: number): IMatch[] {
+		let matches = this.matches;
+		matches.sort((m1, m2) => Date.parse(m1.date) - Date.parse(m2.date))
+		const matchesPlayed: IMatch[] = [];
+		let count: number = 1;
+		for (var i = 0, len = matches.length; i < len; i++) {
+			const match: IMatch = matches[i]
+			if(this.isClubInMatch(match, footballClub)) {
+				matchesPlayed.push(match)
+				count += 1
+			}
+			if(limit) {
+				if(count == limit) {
+					break
+				}
+			}
+		}
+		return matchesPlayed;
+	}
+	isMatchDraw(match: IMatch): boolean {
+		return match.teamAGoals == match.teamBGoals
+	}
+	isTeamWon(match: IMatch, footballClub: IFootballClub): boolean {
+		return this.getWinningTeam(match) == footballClub.clubName
+	}
+	getWinningTeam(match: IMatch) : string{
+		if(match.teamAGoals > match.teamBGoals) {
+			return match.teamA
+		}
+		if(match.teamBGoals > match.teamAGoals) {
+			return match.teamB
+		}
+		return "";
 	}
 	constructor() { }
 
